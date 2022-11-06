805e3b8eec7b59f93e2968f9814d24c63bb0c476
Saurabhsawant8 <sawantsaurabh2399@gmail.com>
2022-11-06 20:36:31 +0530

    Authorisation assignment

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -3,6 +3,7 @@ const userModel = require("../models/userModel");
 
 
 
+// You now have to move this similar code in all the three apis in a suitable middleware
 const midd1=async function(req,res,next){
     const Token=req.headers["x-auth-token"]
     if(!Token) return res.send("mandatory header is missing")
@@ -11,11 +12,11 @@ const midd1=async function(req,res,next){
     if(!decodedToken) return res.send("invalid Token")
 
     const USERid=req.params.userId
-    let user=await userModel.findById(userId)
-     if(!user) return res.send("user not found")
-    console.log(decodedToken)
+    if(decodedToken._id != USERid) return res.send("not authorised")
+    
+    let user=await userModel.findById(USERid)
+    if(!user) return res.send("user not found")
    
-   if(decodedToken._id!=USERid) return res.send("not authorised")
    next()
   }
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,33 +1,8 @@
+const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
 const userController= require("../controllers/userController")
-const jwt = require("jsonwebtoken");
 const commonMW=require("../middleware/middleware")
-
-
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-const midd1=async function(req,res,next){
-    const Token=req.headers["x-auth-token"]
-    if(!Token) return res.send("mandatory header is missing")
-
-    const decodedToken=jwt.verify(Token,"saurabh-secret-key")
-    if(!decodedToken) return res.send("invalid Token")
-
-    let userId=req.params.userId
-    let user=await userModel.findById(userId)
-     if(!user) return res.send("user not found")
-
-  let jak=decodedToken
-  console.log(jak)
-   const USERid=req.params.userId
-   if(decodedToken._id!=USERid) return res.send("not authorised")
-   next()
-}
-
-
 
 
 
@@ -35,5 +10,6 @@ const midd1=async function(req,res,next){
 router.get("/checkAuthorisation/:userId", commonMW.midd1, userController.checkAuthorisation)
 router.put("/updateUser/:userId", commonMW.midd1, userController.updateUser)
 router.delete("/deletUser/:userId", commonMW.midd1, userController.deletUser)
+
 
 module.exports = router;
