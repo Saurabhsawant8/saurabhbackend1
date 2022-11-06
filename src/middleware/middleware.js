df943eb5468fa1e7c1ba6be3ab9af1f731863035
Saurabhsawant8 <sawantsaurabh2399@gmail.com>
2022-11-06 17:41:37 +0530

    authentication assignment

--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -10,13 +10,11 @@ const midd1=async function(req,res,next){
     const decodedToken=jwt.verify(Token,"saurabh-secret-key")
     if(!decodedToken) return res.send("invalid Token")
 
-    let userId=req.params.userId
+    const USERid=req.params.userId
     let user=await userModel.findById(userId)
      if(!user) return res.send("user not found")
-
-  let jak=decodedToken
-  console.log(jak)
-   const USERid=req.params.userId
+    console.log(decodedToken)
+   
    if(decodedToken._id!=USERid) return res.send("not authorised")
    next()
   }
