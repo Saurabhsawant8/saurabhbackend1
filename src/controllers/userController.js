214b6697fbf74172bb6ec6bcbd0ee298d0b1af34
Saurabhsawant8 <sawantsaurabh2399@gmail.com>
2022-11-08 18:18:53 +0530

    try and catch in authorisation assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,29 +6,39 @@ const userModel = require("../models/userModel");
 
 // q.1) fetch user details
 const checkAuthorisation=async function(req,res){
-  
+  try{
      const USERid=req.params.userId
      let useris=await userModel.findById({_id:USERid})
-     res.send({useris})
- 
+     res.status(200).send({status:true,data:useris})
+    } catch(error){
+      res.status(500).send({status:false,msg:error.message})
+    }
 }
 
 
 // q.2) update user
 const updateUser=async function(req,res){
+  try{
   const USERid=req.params.userId
   let data2=req.body
   let updatedata=await userModel.findByIdAndUpdate({_id:USERid},{$set:data2},{new:true})
-  res.send({updatedata})
+  res.status(201).send({status:true,data:updatedata})
+} catch(error){
+  res.status(500).send({status:false,msg:error.message})
+}
 }
 
 
 
 // q.3) delete user apis
 const deletUser=async function(req,res){
+  try{
   const USERid=req.params.userId
   let deletdata=await userModel.findByIdAndUpdate({_id:USERid},{$set:{isDeleted:true}},{new:true})
-  res.send({deletdata})
+  res.status(201).send({status:true,data:deletdata})
+} catch(error){
+  res.status(500).send({status:false,msg:error.message})
+}
 
 }
 
--- src/middleware/middleware.js
+++ src/middleware/middleware.js
@@ -5,20 +5,25 @@ const userModel = require("../models/userModel");
 
 // You now have to move this similar code in all the three apis in a suitable middleware
 const midd1=async function(req,res,next){
+    try{
     const Token=req.headers["x-auth-token"]
-    if(!Token) return res.send("mandatory header is missing")
+    if(!Token) return res.status(400).send({status:false,msg:"mandatory header is missing"})
 
     const decodedToken=jwt.verify(Token,"saurabh-secret-key")
-    if(!decodedToken) return res.send("invalid Token")
+    if(!decodedToken) return res.status(401).send({status:false,msg:"invalid Token"})
 
     const USERid=req.params.userId
-    if(decodedToken._id != USERid) return res.send("not authorised")
+    if(decodedToken._id != USERid) return res.status(403).send({status:false,msg:"not authorised"})
     
     let user=await userModel.findById(USERid)
-    if(!user) return res.send("user not found")
+    if(!user) return res.status(404).send({status:false,msg:"user not found"})
    
    next()
-  }
+
+  } catch(error){
+  res.status(500).send({status:false,msg:error.message})
+}
+}
 
 
   module.exports.midd1=midd1
